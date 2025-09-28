42954e5e468b90ad4c3c24eca0c28ac33d6634fe
HipolitoThe3rd <54292812+HipolitoThe3rd@users.noreply.github.com>
2025-09-28 11:00:00 -0400

    Delete source-code directory

--- source-code/src/main/java/com/example/travelapp/repository/CabBookingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.travelapp.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.example.travelapp.model.CabBooking;
-
-public interface CabBookingRepository extends MongoRepository<CabBooking, String> {
-}
